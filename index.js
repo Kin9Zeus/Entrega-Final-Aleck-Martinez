9fb5cb2055966b95337f635c35cd2800c129c59c
Kin9Zeus <aleckm99@hotmail.com>
2023-07-16 03:14:04 -0500

    Fetch añadido con datos random de clientes random

--- index.js
+++ index.js
@@ -131,3 +131,19 @@ function guardarDatosEnLocalStorage(nombre, email, id, edad) {
     localStorage.setItem('id', id);
     localStorage.setItem('edad', edad.toString());
 }
+
+const API_URL = "http://jsonplaceholder.typicode.com";
+
+fetch(`${API_URL}/users`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
